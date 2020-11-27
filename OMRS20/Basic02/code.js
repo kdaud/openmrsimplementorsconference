d53c31cc4595fd349b89d73bfe32722a7161f2be
kdaud <dkakumirizii@gmail.com>
2020-11-27 15:52:19 +0300

    methods and functions in js

---
+++ OMRS20/Basic02/code.js
@@ -0,0 +1,2 @@
+console.log('Hello Daud Kakumirizi from the College of Computing and Informatics')
+console.log('Graduation is scheduled to take place in march 2021')
--- OMRS20/Qualityteam.js
+++ OMRS20/Qualityteam.js
@@ -1,2 +1,3 @@
 var username='Kaweesi Joseph';
 console.log(username+' '+' is the Quality Assurance Engineer working with Openmrs organisation')
+var myname='John Keneddy'
